98d6bd772ffe68e67ce86a6cd27ca009c00fb3b7
stream.T <145727481+jumpingbob@users.noreply.github.com>
2024-05-16 08:06:08 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from pycaret.classification import load_model
+
 model = load_model('path_to_your_model.pkl')
 feature_importance = model.feature_importances_
 df_feature_importance = pd.DataFrame({'Feature': model.feature_names_, 'Importance': feature_importance})
+st.write(df_feature_importance)
