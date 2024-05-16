2ec9bd304f45429f14ea73c57a863568c3b0d4a8
stream.T <145727481+jumpingbob@users.noreply.github.com>
2024-05-16 08:15:12 +0000

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import pandas as pd
 from pycaret.classification import load_model
+import os
 
 st.title("ああ")
 uploaded_file = st.file_uploader("pklファイルをアップロードしてください。", type=['pkl'])
 
-model = load_model(uploaded_file)
-feature_importance = model.feature_importances_
-df_feature_importance = pd.DataFrame({'Feature': model.feature_names_, 'Importance': feature_importance})
-st.write(df_feature_importance)
+if uploaded_file is not None:
+    with open(os.path.join("temp_model.pkl"), "wb") as f:
+        f.write(uploaded_file.getvalue())
+    model = load_model("temp_model.pkl")
+    feature_importance = model.feature_importances_
+    df_feature_importance = pd.DataFrame({'Feature': model.feature_names_, 'Importance': feature_importance})
+    st.write(df_feature_importance)
