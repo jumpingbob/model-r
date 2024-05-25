aa0c6f45c383384a4b2eb61e23d598d3c3ef019b
stream.T <145727481+jumpingbob@users.noreply.github.com>
2024-05-25 01:02:25 +0000

    aaaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,23 @@ def generate_problem():
         sp.exp(x) / (sp.exp(x) - 3)**2,  # ∫e^x / (e^x - 3)^2 dx
         sp.cos(x)**2 * sp.sin(x),  # ∫cos^2(x)sin(x) dx
         3*x**2 / sp.sqrt(x**3 + 2),  # ∫3x^2 / √(x^3 + 2) dx
+        x**3 / (x**2 - 1),  # ∫x^3 / (x^2 - 1) dx
+        x / (x**2 + 4*x + 4),  # ∫x / (x^2 + 4x + 4) dx
+        (x**3 + 1) / x,  # ∫(x^3 + 1) / x dx
+        x * sp.exp(x**2),  # ∫x e^(x^2) dx
+        sp.sqrt(x**2 + 4*x + 4),  # ∫√(x^2 + 4x + 4) dx
+        x**3 / (x - 3)**2,  # ∫x^3 / (x - 3)^2 dx
+        x**2 * sp.sqrt(x + 1),  # ∫x^2√(x + 1) dx
+        sp.exp(x) / (sp.exp(x) + 1),  # ∫e^x / (e^x + 1) dx
+        sp.sin(x) * sp.cos(2*x),  # ∫sin(x)cos(2x) dx
+        sp.sin(2*x) * sp.cos(x),  # ∫sin(2x)cos(x) dx
+        sp.cos(x)**3 * sp.sin(x),  # ∫cos^3(x)sin(x) dx
+        x**3 / sp.sqrt(x**4 + 4),  # ∫x^3 / √(x^4 + 4) dx
+        1 / (x**3 - x),  # ∫1 / (x^3 - x) dx 部分分数分解
+        1 / (x**2 + 4*x + 4),  # ∫1 / (x^2 + 4*x + 4) dx 部分分数分解
+        sp.sin(x) * sp.cos(x),  # ∫sin(x)cos(x) dx 積和の公式
+        sp.sin(x)**2 * sp.cos(x),  # ∫sin^2(x)cos(x) dx 積和の公式
+        sp.cos(x)**4 * sp.sin(x),  # ∫cos^4(x)sin(x) dx 積和の公式
     ]
     
     # 問題をランダムに選択
@@ -39,14 +56,9 @@ def generate_problem():
 
 # セッション状態の初期化
 if 'problem' not in st.session_state:
-    st.session_state['problem'] = None
+    st.session_state['problem'] = generate_problem()
 if 'solution' not in st.session_state:
     st.session_state['solution'] = None
-
-# 問題を生成
-if st.button("問題を生成"):
-    st.session_state['problem'] = generate_problem()
-    st.session_state['solution'] = None  # 新しい問題を生成する際に解答をリセット
 
 # 問題を表示
 if st.session_state['problem'] is not None:
@@ -61,10 +73,3 @@ if st.session_state['problem'] is not None:
 if st.session_state['solution'] is not None:
     st.write("### 解答")
     st.latex(sp.latex(st.session_state['solution']) + " + C")
-
-
-
-# ボタンを押すと新しい問題を生成
-if st.button("新しい問題を生成"):
-    problem = generate_problem()
-    st.experimental_rerun()
