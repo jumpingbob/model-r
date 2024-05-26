e9b97e06385ee93af5773065c8c9cec9cfa7abc4
stream.T <145727481+jumpingbob@users.noreply.github.com>
2024-05-26 09:45:59 +0000

    テスト

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,75 +1,134 @@
 import streamlit as st
-import sympy as sp
-import random
+import numpy as np
+import plotly.express as px
 
-st.title("積分問題ジェネレーター")
+# ユーザーからの入力を受け取り、それぞれの項目の値をリストに格納する
+def get_user_input(features):
+    user_input = []
+    options = ["5.とても高い", "4.やや高い", "3.ふつう", "2.やや低い", "1.とても低い"]
+    for feature in features:
+        user_input.append(int(st.radio(f"**{feature}**の高さを選択してください", options=options, index=2, key=feature)[0]))  # 文字列の最初の文字を数値に変換
+    return user_input
 
-# xをシンボルとして定義
-x = sp.symbols('x')
+# 逆スコアリングを適用する
+def reverse_scoring(user_input, reverse_indices):
+    for i in reverse_indices:
+        user_input[i] = 6 - user_input[i]  # 1から5の範囲なので、6 - current_valueで逆にする
+    return user_input
 
-# 問題を生成する関数
-def generate_problem():
-    problems = [
-        sp.sin(x) * sp.exp(x),  # ∫sin(x)e^x dx
-        x * sp.log(x),  # ∫x log(x) dx
-        sp.sqrt(x**2 + 1),  # ∫√(x^2 + 1) dx
-        x**2 / (x - 1),  # ∫x^2/(x-1) dx
-        sp.exp(x) / (sp.exp(x) - 1),  # ∫e^x/(e^x - 1) dx
-        1 / (x**2 + x),  # ∫1/(x^2 + x) dx
-        sp.sin(x)**2,  # ∫sin^2(x) dx
-        sp.cos(x)**3,  # ∫cos^3(x) dx
-        sp.cos(3*x) * sp.cos(x),  # ∫cos(3x)cos(x) dx
-        x * sp.exp(-x**2),  # ∫x e^(-x^2) dx
-        (x**2 + 1)**2 / x**3,  # ∫(x^2 + 1)^2 / x^3 dx
-        sp.cos(x) - sp.sin(x),  # ∫(cos(x) - sin(x)) dx
-        (sp.cos(x) - 1) * (sp.cos(x)**2 + sp.cos(x) + 1) / sp.cos(x)**2,  # ∫(cos(x) - 1)(cos^2(x) + cos(x) + 1) / cos^2(x) dx
-        2*x + sp.exp(x),  # ∫(2x + e^x) dx
-        sp.sqrt(2*x - 3),  # ∫√(2x - 3) dx
-        x**2 / (x - 2)**2,  # ∫x^2 / (x - 2)^2 dx
-        x * sp.sqrt(x + 2),  # ∫x√(x + 2) dx
-        sp.exp(x) / (sp.exp(x) - 3)**2,  # ∫e^x / (e^x - 3)^2 dx
-        sp.cos(x)**2 * sp.sin(x),  # ∫cos^2(x)sin(x) dx
-        3*x**2 / sp.sqrt(x**3 + 2),  # ∫3x^2 / √(x^3 + 2) dx
-        x**3 / (x**2 - 1),  # ∫x^3 / (x^2 - 1) dx
-        x / (x**2 + 4*x + 4),  # ∫x / (x^2 + 4x + 4) dx
-        (x**3 + 1) / x,  # ∫(x^3 + 1) / x dx
-        x * sp.exp(x**2),  # ∫x e^(x^2) dx
-        sp.sqrt(x**2 + 4*x + 4),  # ∫√(x^2 + 4x + 4) dx
-        x**3 / (x - 3)**2,  # ∫x^3 / (x - 3)^2 dx
-        x**2 * sp.sqrt(x + 1),  # ∫x^2√(x + 1) dx
-        sp.exp(x) / (sp.exp(x) + 1),  # ∫e^x / (e^x + 1) dx
-        sp.sin(x) * sp.cos(2*x),  # ∫sin(x)cos(2x) dx
-        sp.sin(2*x) * sp.cos(x),  # ∫sin(2x)cos(x) dx
-        sp.cos(x)**3 * sp.sin(x),  # ∫cos^3(x)sin(x) dx
-        x**3 / sp.sqrt(x**4 + 4),  # ∫x^3 / √(x^4 + 4) dx
-        1 / (x**3 - x),  # ∫1 / (x^3 - x) dx 部分分数分解
-        1 / (x**2 + 4*x + 4),  # ∫1 / (x^2 + 4*x + 4) dx 部分分数分解
-        sp.sin(x) * sp.cos(x),  # ∫sin(x)cos(x) dx 積和の公式
-        sp.sin(x)**2 * sp.cos(x),  # ∫sin^2(x)cos(x) dx 積和の公式
-        sp.cos(x)**4 * sp.sin(x),  # ∫cos^4(x)sin(x) dx 積和の公式
+# Zスコア正規化
+def z_score_scaling(user_input):
+    mean_val = np.mean(user_input)
+    std_val = np.std(user_input)
+    #st.write(f"mean_val: {mean_val}, std_val: {std_val}")  # デバッグ出力
+    if std_val == 0:
+        # すべての値が同じ場合、すべてのスケーリング値を0に設定
+        scaled_values = [0 for _ in user_input]
+    else:
+        scaled_values = [(x - mean_val) / std_val for x in user_input]
+    #st.write(f"scaled_values: {scaled_values}")  # デバッグ出力
+    return scaled_values
+
+# ストレスレベルを計算
+def calculate_stress_level(scaled_values, feature_importances):
+    stress_level = np.dot(scaled_values, feature_importances)
+    #st.write(f"stress_level: {stress_level}")  # デバッグ出力
+    return stress_level
+
+# 最も高い項目を特定する
+def find_top_highest_features(scaled_values, features, top_n=3):
+    sorted_indices = np.argsort(scaled_values)[-top_n:][::-1]
+    highest_features = [(features[i], scaled_values[i]) for i in sorted_indices]
+    return highest_features
+
+# Streamlitアプリの実行
+def main():
+    st.title("メンタルヘルスセルフチェッカー")
+
+    # セッション状態を初期化
+    if 'user_inputs' not in st.session_state:
+        st.session_state.user_inputs = []
+
+    st.write("以下のラジオボタンで各項目を評価し、ストレスレベルおよびストレス要因を推定します。")
+    st.markdown("直感的にあてはまる各項目の強さを選択してください。")
+
+    # データを収集した際の質問項目
+    features = [
+        "anxiety level (不安レベル)",
+        "self-esteem (自尊心の高さ)",
+        "depression (うつ傾向)",
+        "headache (頭痛の強度)",
+        "sleep quality (睡眠の質)",
+        "breathing problem (呼吸の苦しさ・息苦しさ)",
+        "noise level (騒音レベル)",
+        "living conditions (生活環境・状態)",
+        "safety (安全性)",
+        "basic needs (基本的欲求)",
+        "academic performance (学業成績)",
+        "study load (学業負担)",
+        "teacher-student relationship (教師と生徒の関係)",
+        "future career concerns (将来のキャリア・進路に関する懸念)",
+        "peer pressure (仲間からのプレッシャー)",
+        "extracurricular activities (課外活動における負担)",
+        "bullying (外部からの圧力的な被害)"
     ]
-    
-    # 問題をランダムに選択
-    problem = random.choice(problems)
-    
-    return problem
 
-# セッション状態の初期化
-if 'problem' not in st.session_state:
-    st.session_state['problem'] = generate_problem()
-if 'solution' not in st.session_state:
-    st.session_state['solution'] = None
+    # 各質問項目の特徴量重要度
+    feature_importances = np.array([
+        0.057802,  # anxiety level
+        0.057088,  # self-esteem
+        0.039464,  # depression
+        0.081881,  # headache
+        0.088141,  # sleep quality
+        0.023011,  # breathing problem
+        0.042920,  # noise level
+        0.026199,  # living conditions
+        0.056138,  # safety
+        0.072368,  # basic needs
+        0.075147,  # academic performance
+        0.035174,  # study load
+        0.051709,  # teacher-student relationship
+        0.067962,  # future career concerns
+        0.048509,  # peer pressure
+        0.074542,  # extracurricular activities
+        0.101947   # bullying
+    ])
 
-# 問題を表示
-if st.session_state['problem'] is not None:
-    st.write("### 問題")
-    st.latex(r"\int " + sp.latex(st.session_state['problem']) + r" \, dx")
-    
-    # 解答を計算
-    if st.button("解答を表示"):
-        st.session_state['solution'] = sp.integrate(st.session_state['problem'], x)
+    user_input = get_user_input(features)
 
-# 解答を表示
-if st.session_state['solution'] is not None:
-    st.write("### 解答")
-    st.latex(sp.latex(st.session_state['solution']) + " + C")
+    st.session_state.user_inputs = user_input  # 入力データをセッション状態に保存
+
+    st.write("入力された値:", st.session_state.user_inputs)
+
+    # 逆スコアリングを適用するインデックス
+    reverse_indices = [1, 4, 7, 8, 9, 10, 12]  # 1: self-esteem, 4: sleep quality, 7: living conditions, 8: safety, 9: basic needs, 10: academic performance, 12: teacher-student relationship
+    user_input = reverse_scoring(st.session_state.user_inputs, reverse_indices)
+
+    st.write("Values after applying reverse scoring:", user_input)
+
+    scaled_values = z_score_scaling(user_input)
+
+    st.write("Z-score normalized values:", scaled_values)
+
+    stress_level = calculate_stress_level(scaled_values, feature_importances)
+
+    stress_level_rounded = round(stress_level, 2)  # 小数第二位まで四捨五入
+
+    st.write("ストレスレベル:", stress_level_rounded)
+
+    highest_features = find_top_highest_features(scaled_values, features, top_n=3)
+    st.write("ストレス要因上位3項目:")
+    for feature, value in highest_features:
+        st.write(f"{feature}: {value}")
+
+    st.write("以下は、ユーザーのストレス要因をレーダーチャートで視覚化したものです。")
+    fig = px.line_polar(
+        r=scaled_values + scaled_values[:1],  # 周期的に閉じるために、最初の値を最後に追加
+        theta=features + features[:1],  # 周期的に閉じるために、最初の項目を最後に追加
+        line_close=True,
+        title="ストレス要因",
+    )
+    st.plotly_chart(fig)
+
+if __name__ == "__main__":
+    main()
